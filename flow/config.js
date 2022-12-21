bc059bba98a5f1d17fcd099a034ef01044b55bb0
lanford33 <lanfordcai@outlook.com>
2022-12-21 13:33:17 +0800

    update deps

--- flow/config.js
+++ flow/config.js
@@ -1,6 +1,6 @@
 import { config } from "@onflow/fcl";
 
 config({
-  "accessNode.api": "https://testnet.onflow.org",
+  "accessNode.api": "https://rest-testnet.onflow.org",
   "discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn",
 })
